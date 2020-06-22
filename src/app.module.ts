0cdb98c85960e53b3640c8c6701825f7efd8398d
ilkaydnc <ilkaydinc1@gmail.com>
2020-06-22 11:49:25 +0300

    Import graphql and mongodb modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { GraphQLModule } from '@nestjs/graphql';
 import { LocationModule } from './location/location.module';
 
 @Module({
-  imports: [LocationModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/worktracking',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [],
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    LocationModule,
+  ],
 })
 export class AppModule {}
