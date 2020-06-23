0fec296c3228d5800eb3b99ba59522a9d6ffdaef
ilkaydnc <ilkaydinc1@gmail.com>
2020-06-23 18:32:33 +0300

    Create UpdatePartnerInput file

---
+++ src/partner/update-partner.input.ts
@@ -0,0 +1,28 @@
+import { InputType, Field, ID } from '@nestjs/graphql';
+import { IsUUID, IsOptional, IsNotEmpty } from 'class-validator';
+
+@InputType()
+export class UpdatePartnerInput {
+  @IsUUID()
+  @IsNotEmpty()
+  @Field()
+  id: string;
+
+  @IsOptional()
+  @Field({ nullable: true })
+  name: string;
+
+  @IsOptional()
+  @Field({ nullable: true })
+  phone: string;
+
+  @IsUUID()
+  @IsOptional()
+  @Field({ nullable: true })
+  locationId: string;
+
+  @IsOptional()
+  @IsUUID('4', { each: true })
+  @Field(() => [ID], { nullable: true })
+  sectorIds: string[];
+}
