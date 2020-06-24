714fc1e526512348d9f48a700d1d73991c9f569a
ilkaydnc <ilkaydinc1@gmail.com>
2020-06-24 19:30:14 +0300

    Add works query, createWork mutation and resolve fields

--- src/work/work.resolver.ts
+++ src/work/work.resolver.ts
@@ -1,4 +1,58 @@
-import { Resolver } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Args,
+  Mutation,
+  Parent,
+  ResolveField,
+} from '@nestjs/graphql';
+import { WorkType } from './work.type';
+import { WorkService } from './work.service';
+import { PartnerService } from 'src/partner/partner.service';
+import { LocationService } from 'src/location/location.service';
+import { SectorService } from 'src/sector/sector.service';
+import { Work } from './work.entity';
+import { FilterWorksInput } from './filter-works.input';
+import { CreateWorkInput } from './create-work.input';
+import { Partner } from 'src/partner/partner.entity';
+import { Location } from 'src/location/location.entity';
+import { Sector } from 'src/sector/sector.entity';
 
-@Resolver('Work')
-export class WorkResolver {}
+@Resolver(of => WorkType)
+export class WorkResolver {
+  constructor(
+    private workService: WorkService,
+    private partnerService: PartnerService,
+    private locationService: LocationService,
+    private sectorService: SectorService,
+  ) {}
+
+  @Query(returns => [WorkType])
+  works(
+    @Args('filterWorksInput') filterWorksInput: FilterWorksInput,
+  ): Promise<Work[]> {
+    return this.workService.getWorks(filterWorksInput);
+  }
+
+  @Mutation(returns => WorkType)
+  createWork(
+    @Args('createWorkInput') createWorkInput: CreateWorkInput,
+  ): Promise<Work> {
+    return this.workService.createWork(createWorkInput);
+  }
+
+  @ResolveField()
+  async partner(@Parent() work: Work): Promise<Partner> {
+    return this.partnerService.getPartnerByID(work.partnerId);
+  }
+
+  @ResolveField()
+  async location(@Parent() work: Work): Promise<Location> {
+    return this.locationService.getLocationByID(work.locationId);
+  }
+
+  @ResolveField()
+  async sector(@Parent() work: Work): Promise<Sector> {
+    return this.sectorService.getSectorByID(work.sectorId);
+  }
+}
