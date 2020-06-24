120e0b75fc15f5d43459bc9eab354c4155ebd284
ilkaydnc <ilkaydinc1@gmail.com>
2020-06-24 19:29:01 +0300

    Import TypeOrm, Partner, Location and Sector modules

--- src/work/work.module.ts
+++ src/work/work.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { WorkService } from './work.service';
 import { WorkResolver } from './work.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Work } from './work.entity';
+import { PartnerModule } from 'src/partner/partner.module';
+import { LocationModule } from 'src/location/location.module';
+import { SectorModule } from 'src/sector/sector.module';
 
 @Module({
-  providers: [WorkService, WorkResolver]
+  imports: [
+    TypeOrmModule.forFeature([Work]),
+    PartnerModule,
+    LocationModule,
+    SectorModule,
+  ],
+  providers: [WorkService, WorkResolver],
 })
 export class WorkModule {}
