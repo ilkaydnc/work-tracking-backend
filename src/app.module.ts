1fa63601e4f8e9181f8d67d2c168ac7cc05ceae1
ilkaydnc <ilkaydinc1@gmail.com>
2020-06-24 19:23:49 +0300

    Import Work entity to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,18 @@ import { GraphQLModule } from '@nestjs/graphql';
 
 import { LocationModule } from './location/location.module';
 import { Location } from './location/location.entity';
+
 import { SectorModule } from './sector/sector.module';
 import { Sector } from './sector/sector.entity';
+
 import { PartnerModule } from './partner/partner.module';
 import { Partner } from './partner/partner.entity';
+
 import { AdModule } from './ad/ad.module';
 import { Ad } from './ad/ad.entity';
+
 import { WorkModule } from './work/work.module';
+import { Work } from './work/work.entity';
 
 @Module({
   imports: [
@@ -19,7 +24,7 @@ import { WorkModule } from './work/work.module';
       url: 'mongodb://localhost/worktracking',
       synchronize: true,
       useUnifiedTopology: true,
-      entities: [Location, Sector, Partner, Ad],
+      entities: [Location, Sector, Partner, Ad, Work],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
