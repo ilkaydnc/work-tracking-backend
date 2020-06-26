1521d2e3dbb4e01d7f364a40bf4d01c456721d8b
ilkaydnc <ilkaydinc1@gmail.com>
2020-06-26 19:35:54 +0300

    Add date and amount fields

--- src/work/work.type.ts
+++ src/work/work.type.ts
@@ -1,25 +1,31 @@
-import { ObjectType, Field, ID } from '@nestjs/graphql';
-import { PartnerType } from 'src/partner/partner.type';
-import { LocationType } from 'src/location/location.type';
-import { SectorType } from 'src/sector/sector.type';
+import { ObjectType, Field, ID, Int } from '@nestjs/graphql'
+import { PartnerType } from 'src/partner/partner.type'
+import { LocationType } from 'src/location/location.type'
+import { SectorType } from 'src/sector/sector.type'
 
 @ObjectType('Work')
 export class WorkType {
   @Field(type => ID)
-  id: string;
+  id: string
 
   @Field(type => PartnerType)
-  partner: string;
+  partner: string
 
   @Field(type => LocationType)
-  location: string;
+  location: string
 
   @Field(type => SectorType)
-  sector: string;
+  sector: string
+
+  @Field(type => Int)
+  amount: number
 
   @Field()
-  amount: number;
+  date: Date
 
   @Field()
-  date: string;
+  created_at: Date
+
+  @Field()
+  updated_at: Date
 }
