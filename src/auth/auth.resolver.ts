418436d7a4e70fcffa8b5d58568784e807bd2cfd
ilkaydnc <ilkaydinc1@gmail.com>
2020-07-10 13:18:35 +0300

    Add me query for get current user

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,13 +1,22 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql'
+import { Resolver, Query, Mutation, Args, Context } from '@nestjs/graphql'
 import { UserType } from './user.type'
 import { AuthService } from './auth.service'
 import { SignUpInput } from './sign-up.input'
 import { User } from './user.entity'
 import { SignInInput } from './sign-in.input'
+import { CurrentUser } from './current-user.decorator'
+import { UseGuards } from '@nestjs/common'
+import { GqlAuthGuard } from './gql-auth.guard'
 
 @Resolver(of => UserType)
 export class AuthResolver {
   constructor(private authService: AuthService) {}
+
+  @Query(returns => UserType)
+  @UseGuards(GqlAuthGuard)
+  me(@CurrentUser() user: User): User {
+    return user
+  }
 
   @Mutation(returns => UserType)
   async signUp(@Args('signUpInput') signUpInput: SignUpInput): Promise<User> {
--- src/auth/current-user.decorator.ts
+++ src/auth/current-user.decorator.ts
@@ -3,5 +3,6 @@ import { GqlExecutionContext } from '@nestjs/graphql'
 
 export const CurrentUser = createParamDecorator((data: unknown, context: ExecutionContext) => {
   const ctx = GqlExecutionContext.create(context)
-  return ctx.getContext().req.user
+  const token = ctx.getContext().req.headers.authorization.split(' ')[1]
+  return { ...ctx.getContext().req.user, token }
 })
